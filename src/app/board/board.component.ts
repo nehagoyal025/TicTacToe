949b5942f8b5f6325762b1a4c16f91777a3ac1fa
saurabhneerja <84119227+saurabhneerja@users.noreply.github.com>
2024-09-04 18:40:09 +0530

    Initial commit

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+  board: string[] = Array(9).fill('');
+  currentPlayer: string = 'X';
+  winner: string | null = null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  makeMove(index: number): void {
+    if (!this.board[index] && !this.winner) {
+      this.board[index] = this.currentPlayer;
+      if (this.checkWinner()) {
+        this.winner = this.currentPlayer;
+      } else {
+        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+      }
+    }
+  }
+
+  checkWinner(): boolean {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    return winningCombinations.some(combination =>
+      combination.every(index => this.board[index] === this.currentPlayer)
+    );
+  }
+
+  resetGame(): void {
+    this.board = Array(9).fill('');
+    this.currentPlayer = 'X';
+    this.winner = null;
+  }
+
+}
